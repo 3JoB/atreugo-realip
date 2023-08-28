a494adcb7be9264b271b54f8eb73ca4de169a33b
Zero <54469706+3JoB@users.noreply.github.com>
2023-08-28 15:06:21 +0800

    fix

--- realip.go
+++ realip.go
@@ -10,16 +10,24 @@ import (
 )
 
 var headers = [...]string{
-	"X-Client-IP",
-	"X-Original-Forwarded-For",
-	"X-Forwarded-For",
 	"CF-Connecting-IP", // Cloudflare
 	"Fastly-Client-Ip", // Fastly CDN and Firebase hosting
 	"True-Client-Ip",   // Akamai and Cloudflare
 	"X-Real-IP",        // Nginx proxy/FastCGI
+	"X-Client-IP",
+	"X-Original-Forwarded-For",
+	"X-Forwarded-For",
 	"X-Forwarded",
 	"Forwarded-For",
 	"Forwarded",
+}
+
+// Only works when using Cloudflare as CDN!
+func County(c *atreugo.RequestCtx) string {
+	if c == nil {
+		return ""
+	}
+	return util.RequestHeader(c, textproto.CanonicalMIMEHeaderKey("Cf-Ipcountry"))
 }
 
 func FromRequest(c *atreugo.RequestCtx) string {
@@ -29,13 +37,18 @@ func FromRequest(c *atreugo.RequestCtx) string {
 
 	for _, h := range headers {
 		val := util.RequestHeader(c, textproto.CanonicalMIMEHeaderKey(h))
-
 		if strings.ContainsRune(val, ',') {
-			for _, address := range strings.Split(val, ",") {
-				address = strings.TrimSpace(address)
-				if isValidPublicAddress(address) {
-					return address
-				}
+			str_sp := strings.Split(val, ",")
+			str_len := len(str_sp)
+			address := ""
+			if str_len > 2 {
+				address = strings.TrimSpace(str_sp[1])
+				
+			} else {
+				address = strings.TrimSpace(str_sp[0])
+			}
+			if isValidPublicAddress(address) {
+				return address
 			}
 		} else {
 			if isValidPublicAddress(val) {
@@ -44,7 +57,7 @@ func FromRequest(c *atreugo.RequestCtx) string {
 		}
 	}
 
-	remoteAddr := c.RemoteAddr().String()
+	remoteAddr := util.RequestHeader(c, textproto.CanonicalMIMEHeaderKey("Remote-Host"))
 	var remoteIP string
 
 	if strings.ContainsRune(remoteAddr, ':') {
